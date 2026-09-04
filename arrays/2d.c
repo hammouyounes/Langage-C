c2422b51e3b980a8ae0c93197154b5b241257047
youness <younesshamou013@gmail.com>
2026-09-04 22:54:31 +0100

    update

--- arrays/2d.c
+++ arrays/2d.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-#include <stdio.h>
 
 int main(){
-    int array[][3]={{1,2,3},{4,5,6},{7,8,9},{10,11,12}};
+    int array[][3]={{1,2,3},{4,5,6},{7,8,9},{10,11,122}};
     for(int i = 0;i < 4;i++){
         for(int j = 0;j < 3;j++){
             printf("%d ",array[i][j]);
--- arrays/test.c
+++ arrays/test.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-void main() {
-    int i = 1;
-    int *p = &i;
-    printf("%d%d%d\n", *p, i++, (*p)++);
+int main() {
+    int x;
+    scanf("%i", &x); 
+    printf("%i", x);
+    return 0;
 }
